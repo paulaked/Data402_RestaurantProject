8abbc694ed8babfc09a1013f6a80c38946b7de09
Jacqueline Ochonma <ochonmajacqueline@gmail.com>
2024-05-13 15:51:07 +0100

    implemented order and remove method

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,20 @@
 class Table:
-    pass
+    def __init__(self, num_diners):
+        self.num_people = num_diners
+        self.bill = []
+
+    #order method
+    def order(self, item, price, quantity=1):
+        for i, bill_i in enumerate(self.bill):
+            if bill_i["item"] ==item and bill_i["price"] == price:
+                self.bill[i]["quantity"] += quantity
+                break
+        else:
+            self.bill.append({"item": item, "price" :price, "quantity"})
+
+
+    #remove method
+
+
+
+
