c3f510321614395a774f4903f9b40af2f4b68455
LuisValrod <luisrodriguezvalido@gmail.com>
2024-05-13 15:47:36 +0100

    order method

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,12 @@
 class Table:
-    pass
+    def __init__(self, num_eaters):
+        self.num_eaters = num_eaters
+        self.bill = []
+
+    def order(self, item, price, quantity=1):
+        for item_in_bill in self.bill:
+            if item_in_bill['item'] == item and item_in_bill['price'] == price:
+                item_in_bill['quantity'] += quantity
+                return
+        self.bill.append({'item': item, 'price': price, 'quantity': quantity})
+
