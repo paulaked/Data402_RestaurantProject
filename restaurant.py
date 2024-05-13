c5410d282acaf6f836b53a6f514f7bf90a2a8ccf
Rajpal Aujla <raujla@spartaglobal.com>
2024-05-13 14:39:28 +0100

    created method names in Table class

--- restaurant.py
+++ restaurant.py
@@ -2,4 +2,20 @@ class Table:
     def __init__(self, num_people):
         pass
 
+
+    def order(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_subtotal(self):
+        pass
+
+    def get_total(self):
+        pass
+
+    def split_bill(self):
+        pass
+
     
