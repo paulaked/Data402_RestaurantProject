942123f6b8d6b5f073ee8b9ed97bf359d74600f2
Sabrina Kaur <kaursabrina25@gmail.com>
2024-05-13 14:54:12 +0100

    Added the constructor and the 'order' method.

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,17 @@
 class Table:
-    pass
+
+    def __init__(self, number_of_people):
+        self.number_of_people = number_of_people
+        self.bill = []
+
+    def order(self, item, price, quantity=1):
+        menu_item = {}
+        if item not in menu_item:
+            menu_item = {"item": item,
+                         "price": price,
+                         "quantity": quantity
+                         }
+            self.bill.append(menu_item)
+        else:
+            quantity += menu_item["quantity"]
+
