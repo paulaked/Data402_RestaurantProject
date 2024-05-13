55b5423fb9535e95d49ea252c4476f8d052e2887
Sabrina Kaur <kaursabrina25@gmail.com>
2024-05-13 17:01:26 +0100

    Added type hints for each method.

--- restaurant.py
+++ restaurant.py
@@ -1,10 +1,10 @@
 class Table:
 
-    def __init__(self, number_of_people):
+    def __init__(self, number_of_people: int):
         self.number_of_people = number_of_people
         self.bill = []
 
-    def order(self, item, price, quantity=1):
+    def order(self, item: str, price: int, quantity: int = 1) -> list:
         menu_item = {}
         if item not in menu_item:
             menu_item = {"item": item,
@@ -17,7 +17,7 @@ class Table:
 
         return self.bill
 
-    def remove(self, item, price, quantity):
+    def remove(self, item: str, price: int, quantity: int) -> bool:
         # iterating through each dict in list
         for menu_item in self.bill:
             # checking if the item and the price match in dict
@@ -33,14 +33,14 @@ class Table:
             else:
                 return False
 
-    def get_subtotal(self):
+    def get_subtotal(self) -> float:
         total_cost = 0
         for menu_item in self.bill:
             total_cost += menu_item["price"] * menu_item["quantity"]
 
         return total_cost
 
-    def get_total(self, service_charge=0.1):
+    def get_total(self, service_charge: float = 0.1) -> dict:
         subtotal = Table.get_subtotal(self)
         service_charge = subtotal * service_charge
         total = subtotal + service_charge
@@ -51,7 +51,7 @@ class Table:
                       }
         return total_dict
 
-    def split_bill(self):
+    def split_bill(self) -> float:
         total = Table.get_subtotal(self)
         subtotal_cost = round(total / self.number_of_people, 2)
 
