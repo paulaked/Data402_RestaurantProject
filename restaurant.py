cd6555d7d994376ccd2f27c0fb99c3d9008be70e
Kyrun Philip-Lessells <kphilip-lessells@spartaglobal.com>
2024-05-13 14:47:40 +0100

    defined methods and variables

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,21 @@
 class Table:
-    pass
+
+    bill = []
+
+    def __init__(self, people: int) -> None:
+        self.people = people
+
+    def order(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_subtotal(self):
+        pass
+
+    def get_total(self):
+        pass
+
+    def split_bill(self):
+        pass
