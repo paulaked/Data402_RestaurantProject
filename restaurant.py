19c557b2c84ba52adc0d1ec9ed33486895f3aa83
Rajpal Aujla <raujla@spartaglobal.com>
2024-05-13 14:55:17 +0100

    added the order function

--- restaurant.py
+++ restaurant.py
@@ -5,9 +5,13 @@ class Table: # represents each table at restaurant
         self.num_people = num_people # store number of people at table
         pass
 
-
-    def order(self):
-        pass
+    def order(self, item, price, quantity=1): #default quantity of 1
+        for i in range(len(self.bill)): #interate through bill
+            if self.bill[i]['item'] == item and self.bill[i]['price'] == price: # check if current item already on bill
+                self.bill[i]['quantity'] += quantity #increment if so
+                break
+        else: # if current item is not on bill
+            self.bill.append({'item': item, 'price': price, 'quantity': quantity}) # add new dictionary with new items to bill list
 
     def remove(self):
         pass
