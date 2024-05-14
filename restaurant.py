3379691d212d2440074a5af0c08a2bd638d33526
Kazim Raza <kazim.r.shah@gmail.com>
2024-05-14 09:28:02 +0100

    Remove Method

--- restaurant.py
+++ restaurant.py
@@ -1,7 +1,7 @@
 class Table:
     def __init__(self, num_of_people):
-        self.bill = []
         self.num_of_people = num_of_people
+        self.bill = []
 
     def order(self, item: str, price: int, quantity=1):
         for item_entry in self.bill:
@@ -10,3 +10,15 @@ class Table:
                 return
 
             self.bill.append({"item": item, "price": price, "quantity": quantity})
+
+    def remove(self, item: str, price: int, quantity=1):
+        for item_entry in self.bill:
+            if item_entry["item"] == item and item_entry["price"] == price:
+                if quantity < item_entry["quantity"]:
+                    item_entry["quantity"] -= quantity
+                    if item_entry["quantity"] == 0:
+                        self.bill.remove({item_entry})
+                    return True
+                else:
+                        return False
+        return False
