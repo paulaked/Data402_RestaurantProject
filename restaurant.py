a2ba17ae6b0ba6cbeb3b06a7cdf539a6a05051de
Kazim Raza <kazim.r.shah@gmail.com>
2024-05-14 09:18:18 +0100

    Order Method

--- restaurant.py
+++ restaurant.py
@@ -2,3 +2,11 @@ class Table:
     def __init__(self, num_of_people):
         self.bill = []
         self.num_of_people = num_of_people
+
+    def order(self, item: str, price: int, quantity=1):
+        for item_entry in self.bill:
+            if item_entry["item"] == item and item_entry["price"] == price:
+                item_entry["quantity"] += quantity
+                return
+
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
